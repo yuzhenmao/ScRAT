19f588a773311db0040b80fc3d386f996ccb2bc7
yuzhenmao <57878927+yuzhenmao@users.noreply.github.com>
2022-07-23 17:50:35 -0700

    Create Transformer.py

---
+++ Transformer.py
@@ -0,0 +1,234 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, input_dim, num_heads, dim_feedforward, dropout=0.0):
+        """
+        Inputs:
+            input_dim - Dimensionality of the input
+            num_heads - Number of heads to use in the attention block
+            dim_feedforward - Dimensionality of the hidden layer in the MLP
+            dropout - Dropout probability to use in the dropout layers
+        """
+        super().__init__()
+
+        # Attention layer
+        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads)
+
+        # Two-layer MLP
+        self.linear_net = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim)
+        )
+
+        # Layers to apply in between the main layers
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        # Attention part
+        attn_out = self.self_attn(x, mask=mask)
+        x = x + self.dropout(attn_out)
+        x = self.norm1(x)
+
+        # MLP part
+        linear_out = self.linear_net(x)
+        x = x + self.dropout(linear_out)
+        x = self.norm2(x)
+
+        return x
+
+
+class TransformerEncoder(nn.Module):
+
+    def __init__(self, num_layers, **block_args):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderBlock(**block_args) for _ in range(num_layers)])
+
+    def forward(self, x, mask=None):
+        for l in self.layers:
+            x = l(x, mask=mask)
+        return x
+
+    def get_attention_maps(self, x, mask=None):
+        attention_maps = []
+        for l in self.layers:
+            _, attn_map = l.self_attn(x, mask=mask, return_attention=True)
+            attention_maps.append(attn_map)
+            x = l(x)
+        return attention_maps
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, max_len=5000):
+        """
+        Inputs
+            d_model - Hidden dimensionality of the input.
+            max_len - Maximum length of a sequence to expect.
+        """
+        super().__init__()
+
+        # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+
+        # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
+        # Used for tensors that need to be on the same device as the module.
+        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
+        self.register_buffer('pe', pe, persistent=False)
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.size(1)]
+        return x
+
+
+class TransformerPredictor(pl.LightningModule):
+
+    def __init__(self, input_dim, model_dim, num_classes, num_heads, num_layers, dropout=0.0,
+                 input_dropout=0.0, pca=False):
+        """
+        Inputs:
+            input_dim - Hidden dimensionality of the input
+            model_dim - Hidden dimensionality to use inside the Transformer
+            num_classes - Number of classes to predict per sequence element
+            num_heads - Number of heads to use in the Multi-Head Attention blocks
+            num_layers - Number of encoder blocks to use.
+            lr - Learning rate in the optimizer
+            warmup - Number of warmup steps. Usually between 50 and 500
+            max_iters - Number of maximum iterations the model is trained for. This is needed for the CosineWarmup scheduler
+            dropout - Dropout to apply inside the model
+            input_dropout - Dropout to apply on the input features
+        """
+        super().__init__()
+        self.save_hyperparameters()
+        self._create_model()
+
+    def _create_model(self):
+        # Input dim -> Model dim
+        self.input_net = nn.Sequential(
+            nn.Dropout(self.hparams.input_dropout),
+            nn.Linear(self.hparams.input_dim, self.hparams.model_dim)
+        )
+        self.dimRedu_net = torch.nn.Sequential(
+            nn.Dropout(self.hparams.input_dropout),
+            nn.Linear(self.hparams.input_dim, self.hparams.model_dim * 2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(self.hparams.dropout),
+            nn.Linear(self.hparams.model_dim * 2, self.hparams.model_dim)
+        )
+        # Positional encoding for sequences
+        self.positional_encoding = PositionalEncoding(d_model=self.hparams.model_dim)
+        # Transformer
+        self.transformer = TransformerEncoder(num_layers=self.hparams.num_layers,
+                                              input_dim=self.hparams.model_dim,
+                                              dim_feedforward=2*self.hparams.model_dim,
+                                              num_heads=self.hparams.num_heads,
+                                              dropout=self.hparams.dropout)
+        # Output classifier per sequence lement
+        self.output_net = nn.Sequential(
+            nn.Linear(self.hparams.model_dim, self.hparams.model_dim),
+            nn.LayerNorm(self.hparams.model_dim),
+            nn.ReLU(inplace=True),
+            nn.Dropout(self.hparams.dropout),
+            nn.Linear(self.hparams.model_dim, self.hparams.num_classes)
+        )
+
+    def forward(self, x, mask=None, add_positional_encoding=False):
+        """
+        Inputs:
+            x - Input features of shape [Batch, SeqLen, input_dim]
+            mask - Mask to apply on the attention outputs (optional)
+            add_positional_encoding - If True, we add the positional encoding to the input.
+                                      Might not be desired for some tasks.
+        """
+        if not self.hparams.pca:
+            x = self.dimRedu_net(x)
+        else:
+            x = self.input_net(x)
+        if add_positional_encoding:
+            x = self.positional_encoding(x)
+        x = self.transformer(x, mask=mask)
+        x = x.mean(1)
+        x = self.output_net(x)
+        return x
+
+    @torch.no_grad()
+    def get_attention_maps(self, x, mask=None, add_positional_encoding=True):
+        """
+        Function for extracting the attention matrices of the whole Transformer for a single batch.
+        Input arguments same as the forward pass.
+        """
+        x = self.input_net(x)
+        if add_positional_encoding:
+            x = self.positional_encoding(x)
+        attention_maps = self.transformer.get_attention_maps(x, mask=mask)
+        return attention_maps
