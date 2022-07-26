b26af0a542fad13d69b472d781f0a706af9ce1df
yuzhenmao <57878927+yuzhenmao@users.noreply.github.com>
2022-07-26 16:20:42 -0700

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -3,28 +3,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
+import pdb
 
 
-def scaled_dot_product(q, k, v, mask=None):
+def scaled_dot_product(q, k, v, mask=None, dropout=None):
     d_k = q.size()[-1]
     attn_logits = torch.matmul(q, k.transpose(-2, -1))
     attn_logits = attn_logits / math.sqrt(d_k)
     if mask is not None:
-        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+        mask = mask.unsqueeze(1).unsqueeze(1)
+        attn_logits = attn_logits.masked_fill(mask, float("-inf"))
     attention = F.softmax(attn_logits, dim=-1)
+    if dropout is not None:
+        attention = dropout(attention)
     values = torch.matmul(attention, v)
     return values, attention
 
 
 class MultiheadAttention(nn.Module):
 
-    def __init__(self, input_dim, embed_dim, num_heads):
+    def __init__(self, input_dim, embed_dim, num_heads, dropout):
         super().__init__()
         assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
 
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.head_dim = embed_dim // num_heads
+        self.dropout = nn.Dropout(dropout)
 
         # Stack all weight matrices 1...h together for efficiency
         # Note that in many implementations you see "bias=False" which is optional
@@ -50,7 +55,7 @@ class MultiheadAttention(nn.Module):
         q, k, v = qkv.chunk(3, dim=-1)
 
         # Determine value outputs
-        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values, attention = scaled_dot_product(q, k, v, mask=mask, dropout=self.dropout)
         values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
         values = values.reshape(batch_size, seq_length, embed_dim)
         o = self.o_proj(values)
@@ -74,7 +79,7 @@ class EncoderBlock(nn.Module):
         super().__init__()
 
         # Attention layer
-        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads, dropout)
         self.norm_first = norm_first
 
         # Two-layer MLP
