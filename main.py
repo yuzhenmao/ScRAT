eee10ac4408b0e638c482e882813e0b0d3065058
Yuzhen Mao <57878927+yuzhenmao@users.noreply.github.com>
2023-06-06 11:15:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -275,7 +275,10 @@ def train(x_train, x_valid, x_test, y_train, y_valid, y_test, id_train, id_test,
 if args.model != 'Transformer':
     args.repeat = 60
 
-p_idx, labels_, cell_type, patient_id, data, cell_type_large = Covid_data(args)
+if args.task != 'custom':
+    p_idx, labels_, cell_type, patient_id, data, cell_type_large = Covid_data(args)
+else:
+    p_idx, labels_, cell_type, patient_id, data, cell_type_large = Custom_data(args)
 rkf = RepeatedKFold(n_splits=abs(args.n_splits), n_repeats=args.repeat * 100, random_state=args.seed)
 num = np.arange(len(p_idx))
 accuracy, aucs, cms, recalls, precisions = [], [], [], [], []
