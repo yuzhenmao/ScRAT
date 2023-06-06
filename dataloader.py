3c61e0e700a05875b5ca60b01662cf41dcf0f7b7
Yuzhen Mao <57878927+yuzhenmao@users.noreply.github.com>
2023-06-06 11:32:55 -0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import scanpy
 
 
 def Covid_data(args):
@@ -164,6 +165,58 @@ def Covid_data(args):
                         continue
                     p_idx.append(tt_idx)
                     l_dict[labels_[idx[0]]] = l_dict.get(labels_[idx[0]], 0) + 1
+
+    # print(l_dict)
+
+    return p_idx, labels_, cell_type, patient_id, origin, cell_type_large
+
+
+def Custom_data(args):
+    '''
+    !!! Need to change line 11 before running the code !!!
+    '''
+    id_dict = {}  
+    data = scanpy.read_h5ad(args.dataset)
+    if args.pca == True:
+        origin = data.obsm['X_pca']
+    else:
+        origin = data.layers['raw']
+    
+    patient_id = data.obs['patient_id']
+
+    labels = data.obs['Outcome']
+
+    cell_type = data.obs['cell_type']
+
+    cell_type_large = None
+    # This (high resolution) cell_type is only for attention analysis, not necessary
+    # cell_type_large = data.obs['cell_type_large']
+
+    labels_ = np.array(labels.map(id_dict))
+
+    l_dict = {}
+    indices = np.arange(origin.shape[0])
+    p_ids = sorted(set(patient_id))
+    p_idx = []
+
+    for i in p_ids:
+        idx = indices[patient_id == i]
+        if len(set(labels_[idx])) > 1:   # one patient with more than one labels
+            for ii in sorted(set(labels_[idx])):
+                if ii > -1:
+                    iidx = idx[labels_[idx] == ii]
+                    tt_idx = iidx
+                    if len(tt_idx) < 500:  # exclude the sample with the number of cells fewer than 500
+                        continue
+                    p_idx.append(tt_idx)
+                    l_dict[labels_[iidx[0]]] = l_dict.get(labels_[iidx[0]], 0) + 1
+        else:
+            if labels_[idx[0]] > -1:
+                tt_idx = idx
+                if len(tt_idx) < 500:  # exclude the sample with the number of cells fewer than 500
+                    continue
+                p_idx.append(tt_idx)
+                l_dict[labels_[idx[0]]] = l_dict.get(labels_[idx[0]], 0) + 1
 
     # print(l_dict)
 
